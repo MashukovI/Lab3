1a66f6be766b5463e211e56401fc485be83dc2c2
IvanMa <ivan03.00@mail.ru>
2024-04-04 22:11:22 +0500

    Создание консольеого приложения для вычисления площади поверхности целиндра, с данными полученными от пользователя

--- TRPO-Lab3.ConsoleApp/Program.cs
+++ TRPO-Lab3.ConsoleApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TRPO_Lab3.Lib;
+
+namespace TRPO_Lab3.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r;
+            Console.WriteLine("Введите радиус цилиндра");
+            while (!double.TryParse(Console.ReadLine(), out r))
+            {
+                Console.WriteLine("Ошибка ввода!");
+            }
+            double h;
+            Console.WriteLine("Введите высоту цилиндра");
+            while (!double.TryParse(Console.ReadLine(), out h))
+            {
+                Console.WriteLine("Ошибка ввода!");
+            }
+
+            Console.WriteLine("Площадь поверхности цилиндра\n" + new Cylinder().SurfArea(r, h));
+        }
+    }
+}
