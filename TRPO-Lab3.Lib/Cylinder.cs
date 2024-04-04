69a9ec632ded435d77a026aa753ebf90a4ce9cfa
IvanMa <ivan03.00@mail.ru>
2024-04-04 15:32:41 +0500

    Модификация кода для добавления исключений и добавление тестов при получении отрицательного числа

--- TRPO-Lab3.Lib/Cylinder.cs
+++ TRPO-Lab3.Lib/Cylinder.cs
@@ -11,7 +11,15 @@ namespace TRPO_Lab3.Lib
         const double pi = Math.PI;
         public double SurfArea(double r, double h)
         {
-            return (2 * pi * r * (h + r));
+            if (r < 0 || h < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else
+            {
+                return 2 * pi * r * (h + r);
+            }
+            
         }
     }
 }
--- TRPO-Lab3.Tests/AreaTest.cs
+++ TRPO-Lab3.Tests/AreaTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TRPO_Lab3.Lib;
 
 namespace TrpoLab3.Tests
 {
@@ -16,6 +17,16 @@ namespace TrpoLab3.Tests
             var result = new TRPO_Lab3.Lib.Cylinder().SurfArea(r, h);
 
             Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void SurfAreaExcTest()
+        {
+            const double r = -6;
+            const double h = 7;
+            Cylinder cylinder = new Cylinder();
+            Assert.Throws<ArgumentOutOfRangeException>(() => cylinder.SurfArea(r, h));
+
         }
     }
 }
