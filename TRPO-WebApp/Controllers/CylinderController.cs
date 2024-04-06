a9e10948f7743ed61b720ea306ae3e5a2b944c07
MashukovI <ivan03.00@mail.ru>
2024-04-06 05:45:50 +0500

    Создание Web-приложения на основе ASP.NET с использованием MVC

---
+++ TRPO-WebApp/Controllers/CylinderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TRPO_WebApp.Models;
+
+namespace TRPO_WebApp.Controllers
+{
+
+    public class CylinderController : Controller
+    {
+        public IActionResult Calculate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(IFormCollection collection)
+        {
+            var cylinder = new CylinderModel
+            {
+                Radius = Convert.ToDouble(collection["Radius"]),
+                Height = Convert.ToDouble(collection["Height"])
+            };
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(cylinder);
+            }
+            var surfaceArea = cylinder.GetSurfaceArea();
+            ViewBag.SurfaceArea = surfaceArea;
+
+            return View(cylinder);
+        }
+    }
+}
---
+++ TRPO-WebApp/Models/CylinderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TRPO_Lab3.Lib;
+
+namespace TRPO_WebApp.Models
+{
+    public class CylinderModel
+    {
+        [Range(0, double.MaxValue, ErrorMessage = "Радиус должен быть больше или равен 0")]
+        public double Radius { get; set; }
+        [Range(0, double.MaxValue, ErrorMessage = "Высота должна быть больше или равна 0")]
+        public double Height { get; set; }
+
+        public double GetSurfaceArea()
+        {
+            Cylinder a = new Cylinder();
+            return a.SurfArea(Radius, Height);
+        }
+    }
+}
